e813f21c70807c1b8da72872dc444bf3afe958d6
Simone Trubian <simone.trubian@gmail.com>
2016-01-19 21:17:33 +0000

    Added more code-checking plugins to the clojure conf file.

--- profiles.clj
+++ profiles.clj
@@ -1,1 +1,5 @@
-{:user {:plugins [[lein-ancient "0.6.8"]]}}
+{:user {:plugins [[lein-ancient "0.6.8"]
+                  [lein-kibit "0.1.2"]
+                  [jonase/eastwood "0.2.3"]
+                  [venantius/yagni "0.1.4"]
+                  [lein-bikeshed "0.2.0"]]}}
