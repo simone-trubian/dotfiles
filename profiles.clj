03d271ee2a41469c20bc5dae202fbcb100c15b57
Simone Trubian <simone.trubian@ondait.com>
2016-06-27 09:59:23 +0100

    Removed Clojure configuration file

--- profiles.clj
+++
@@ -1,5 +0,0 @@
-{:user {:plugins [[lein-ancient "0.6.8"]
-                  [lein-kibit "0.1.2"]
-                  [jonase/eastwood "0.2.3"]
-                  [venantius/yagni "0.1.4"]
-                  [lein-bikeshed "0.2.0"]]}}
