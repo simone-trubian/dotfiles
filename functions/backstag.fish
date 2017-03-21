ed1113b82d31666d17ad027d9c41ff7a46501baa
Simone Trubian <simone.trubian@ondait.com>
2017-03-21 10:51:04 +0000

    Using /etc/hosts host names instead of IP addresses

---
+++ functions/backstag.fish
@@ -0,0 +1,4 @@
+# Phrasee generation backend staging server
+function backstag
+  ssh -i ~/.ssh/generator.pem ubuntu@backstag
+end
---
+++ functions/cdmast.fish
@@ -0,0 +1,4 @@
+# Phrasee continuous integration master sever
+function cdmast
+  ssh -i ~/.ssh/parry-root.pem ubuntu@cdmast
+end
