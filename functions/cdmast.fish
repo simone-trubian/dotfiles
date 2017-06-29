88dfc17c0414b5a9149467fb1f491d2314eeaf69
Simone Trubian <simone.trubian@ondait.com>
2017-06-29 12:05:09 +0100

    Changed the name of CD in CI

--- functions/cdmast.fish
+++
@@ -1,4 +0,0 @@
-# Phrasee continuous integration master sever
-function cdmast
-  ssh -i ~/.ssh/parry-root.pem ubuntu@cdmast
-end
---
+++ functions/cimast.fish
@@ -0,0 +1,4 @@
+# Phrasee continuous integration master sever
+function cimast
+  ssh -i ~/.ssh/parry-root.pem ubuntu@cimast
+end
