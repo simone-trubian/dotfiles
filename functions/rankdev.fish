ed1eee09cc9b61009fbee7c577ff6b644586f221
Simone Trubian <simone@phrasee.co>
2018-03-07 11:58:59 +0000

    Added ranking dev

---
+++ functions/rankdev.fish
@@ -0,0 +1,4 @@
+# Phrasee subject line ranking development server
+function rankdev
+    ssh -i ~/.ssh/generator.pem ubuntu@rankdev
+end
---
+++ functions/webtest.fish
@@ -0,0 +1,4 @@
+# Phrasee web application testing server
+function webtest
+  ssh -i ~/.ssh/sandbox-root.pem ec2-user@webtest
+end
